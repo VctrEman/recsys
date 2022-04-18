bc68e014d4376cd06025aa502cb38bd3c32af7bc
VctrEman <rotcivemanuelms@gmail.com>
2022-04-18 10:16:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify, render_template
+from flask_basicauth import BasicAuth
+import pickle
+import os
+import pandas as pd
+from scipy.sparse import csr_matrix
+
+app = Flask(__name__)
+#app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+#app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+#basic_auth = BasicAuth(app)
+
+modelo = pickle.load(open('models/knn_model.sav','rb'))
+movies = pd.read_pickle("data/processed/movies.pkl")
+MovieDataset = pd.read_pickle("data/processed/FinalMoviesDataset.pkl")
+csr_data = csr_matrix(MovieDataset.values)
+MovieDataset.reset_index(inplace=True)
+
+@app.route('/')
+def home(): 
+    return """App for movie recommention\n
+                    If you liked this movie, you may also like the following movies
+                    to enter a movie type: /movie"""
+
+@app.route('/<movie_name>' ,methods=['GET'])
+def get_movie_recommendation(movie_name,):
+    n_movies_to_reccomend = 10
+    movie_list = movies[movies['title'].str.contains(movie_name)]
+    if len(movie_list):        
+        movie_idx= movie_list.iloc[0]['movieId']
+        movie_idx = MovieDataset[MovieDataset['movieId'] == movie_idx].index[0]
+        distances , indices = modelo.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)    
+        rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
+        recommend_frame = []
+        for val in rec_movie_indices:
+            movie_idx = MovieDataset.iloc[val[0]]['movieId']
+            idx = movies[movies['movieId'] == movie_idx].index
+            recommend_frame.append({'Title':movies.iloc[idx]['title'].values[0],'Distance':val[1]})
+        df = pd.DataFrame(recommend_frame,index=range(1,n_movies_to_reccomend+1))
+        return jsonify(df.to_dict())
+    else:
+        return "No movies found. Please check your input"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='movie recsys based on movielens dataset',
+    author='victor.emanuel',
+    license='',
+)
